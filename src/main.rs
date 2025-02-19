162bb099c43d9c7ffa329cd352715cb6c209a1c6
Kylong <kylong@zjl.me>
2025-02-20 01:36:20 +0800

    feat: 🎸 set up a hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>  {
     println!("Hello, world!");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { "Hello, world!" }))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
